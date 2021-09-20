d9ced3ba3d2a67b4915bca80dc3eb9ca4a0785c7
Денис Соколов <sokolovdenisost@gmail.com>
2021-09-20 15:04:15 +0300

    heroku

---
+++ frontend/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+            plugins: ['@babel/proposal-class-properties', '@babel/proposal-object-rest-spread'],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.jsx', '.css', '.png', '.jpg'],
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+    }),
+  ],
+};
