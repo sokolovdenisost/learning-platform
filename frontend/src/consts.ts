6498068265f35b68e2868b3c6e4a7dc89c39c5f6
Денис Соколов <sokolovdenisost@gmail.com>
2021-09-20 13:50:47 +0300

    add api url in heroku

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,1 +1,2 @@
-export const API_URL: string = 'http://localhost:3001';
+// export const API_URL: string = 'http://localhost:3001';
+export const API_URL: string = 'https://learning-platform-6y6exa.herokuapp.com/';
